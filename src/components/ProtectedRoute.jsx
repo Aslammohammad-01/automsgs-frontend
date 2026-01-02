d51aeb6e21436445f94ee56af06871a01c46d42d
aslammohammad-01 <aslam.mohammad8800@gmail.com>
2026-01-02 19:15:42 +0530

    dummy

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,3 +3,11 @@ export default function ProtectedRoute({ children }) {
 
   return children;
 }
+export default function ProtectedRoute({ children }) {
+  return (
+    <div>
+      <h1>ProtectedRoute working</h1>
+      {children}
+    </div>
+  );
+}
